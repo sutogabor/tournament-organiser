c85ade08cb6bed15fdb4e1bf1045a2b8fc13ac63
TwiceG <g0stron0g@gmail.com>
2023-05-30 14:31:32 +0200

    setup basic filestructure

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.debug = True
+
+# adding configuration for using a sqlite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run()
