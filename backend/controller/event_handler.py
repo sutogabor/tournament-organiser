5db0f1b05ed011ee40036aa8d5a6598a4ab2cde4
sutogabor <wakacorpz@gmail.com>
2023-06-06 11:49:37 +0200

    add all existing routes to main.py, delete routes.py and model package and disable code in event_handler.py

--- backend/controller/event_handler.py
+++ backend/controller/event_handler.py
@@ -1,34 +1,32 @@
-from flask import request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from ..model import model
-from ..main import app
-
-from sqlalchemy.orm import Session
-
-
-db = SQLAlchemy()
-
-
-@app.route('/event', methods=['GET'])
-def get_events():
-    events = db.session.query().all()
-    event_list = []
-    for event in events:
-        event_data = {
-            'id': event.id,
-            'name': event.name,
-            'date': event.date.strftime('%Y-%m-%d')
-        }
-        event_list.append(event_data)
-    return jsonify(event_list)
-
-
-@app.route('/event/add', methods=['POST'])
-def add_event():
-    added_event = request.get_json()
-    event = model.Event(name=added_event['name'], date=added_event['date'])
-    db.session.add(event)
-    db.session.commit()
-    return jsonify({'message': 'Event created successfully.'}), 201
+# from flask import request, jsonify
+# from flask_sqlalchemy import SQLAlchemy
+# from ..model import model
+# from ..main import app
+#
+#
+# db = SQLAlchemy()
+#
+#
+# @app.route('/event', methods=['GET'])
+# def get_events():
+#     events = db.session.query().all()
+#     event_list = []
+#     for event in events:
+#         event_data = {
+#             'id': event.id,
+#             'name': event.name,
+#             'date': event.date.strftime('%Y-%m-%d')
+#         }
+#         event_list.append(event_data)
+#     return jsonify(event_list)
+#
+#
+# @app.route('/event/add', methods=['POST'])
+# def add_event():
+#     added_event = request.get_json()
+#     event = model.Event(name=added_event['name'], date=added_event['date'])
+#     db.session.add(event)
+#     db.session.commit()
+#     return jsonify({'message': 'Event created successfully.'}), 201
 
 
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from database_config import db_name
 from flask_sqlalchemy import SQLAlchemy
-
-
+import model
 app = Flask(__name__)
 
 
@@ -14,10 +13,33 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
+@app.route('/event', methods=['GET'])
+def get_events():
+    events = db.session.query(model.Event).all()
+    event_list = []
+    for event in events:
+        event_data = {
+            'id': event.id,
+            'name': event.name,
+            'date': event.date.strftime('%Y-%m-%d')
+        }
+        event_list.append(event_data)
+    return jsonify(event_list)
+
+
+@app.route('/event/add', methods=['POST'])
+def add_event():
+    added_event = request.get_json()
+    event = model.Event(name=added_event['name'], date=added_event['date'])
+    db.session.add(event)
+    db.session.commit()
+    return jsonify({'message': 'Event created successfully.'}), 201
+
+
 if __name__ == "__main__":
     app.run(
+        port=8080,
         debug=True,
-        host='localhost',
-        port=8080
+        host='localhost'
     )
 
