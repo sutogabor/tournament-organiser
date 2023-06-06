a5ee756bb20d1463ce6510d7489860a1d9c6a27e
sutogabor <wakacorpz@gmail.com>
2023-06-06 13:41:05 +0200

    add get_event route and functionality to main.py

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,29 @@ def add_event():
     return jsonify({'message': 'Event created successfully.'}), 201
 
 
+@app.route('/event/delete/<int:event_id>', methods=['POST'])
+def delete_event(event_id):
+    event = model.Event.query.get(event_id)
+    if not event:
+        return jsonify({'message': 'Event not found.'}), 404
+    db.session.delete(event)
+    db.session.commit()
+    return jsonify({'message': 'Event deleted successfully.'})
+
+
+@app.route('/event/<int:event_id>', methods=['GET'])
+def get_event(event_id):
+    event = model.Event.query.get(event_id)
+    if not event:
+        return jsonify({'message': 'Event not found.'}), 404
+    event_data = {
+        'id': event.id,
+        'name': event.name,
+        'date': event.date.strftime('%Y-%m-%d')
+    }
+    return jsonify(event_data)
+
+
 if __name__ == "__main__":
     app.run(
         port=8080,
