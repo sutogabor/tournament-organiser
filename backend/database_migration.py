18b0ee3f08c241beea1a07b05a9f8631a9390289
sutogabor <wakacorpz@gmail.com>
2023-06-13 10:26:45 +0200

    add Match class to models.py

--- backend/database_migration.py
+++ backend/database_migration.py
@@ -1,4 +1,4 @@
 from database_config import engine
-from model import Base
+from models import Base
 
 Base.metadata.create_all(bind=engine)
--- backend/models.py
+++ backend/models.py
@@ -24,3 +24,12 @@ class Event(Base):
     name = Column(String(255))
     date = Column(Date)
     players = relationship("Player", secondary="player_event", back_populates="events")
+
+
+class Match(Base):
+    __tablename__ = "matches"
+    id = Column(Integer, primary_key=True)
+    player_1_id = Column(Integer, ForeignKey("players.id"), primary_key=True)
+    player_2_id = Column(Integer, ForeignKey("players.id"), primary_key=True)
+    event_id = Column(Integer, ForeignKey("events.id"), primary_key=True)
+    winner = Column(Integer, ForeignKey("players.id"), primary_key=True)
