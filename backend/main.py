9fbc21d36269b0786320fb588abdb2977b003df3
sutogabor <wakacorpz@gmail.com>
2023-06-13 13:28:27 +0200

    add create_app function to main.py and fix imports in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,19 @@
-from flask import Flask, jsonify, request, Blueprint
+from flask import Flask
 from database_config import db_name
-from flask_sqlalchemy import SQLAlchemy
+from routes import routes_bp
+from models import db
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_name
-
-bp = Blueprint("app", __name__)
-
-db = SQLAlchemy()
-db.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_name
+    app.register_blueprint(routes_bp)
+    db.init_app(app)
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(
         port=8080,
         debug=True,
