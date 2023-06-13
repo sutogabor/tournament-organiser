372dd22efe43e4d2613f453be0fe2f359b88f561
sutogabor <wakacorpz@gmail.com>
2023-06-13 10:28:26 +0200

    refactor routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -5,7 +5,7 @@ from main import bp, db
 
 @bp.route("/event", methods=['GET'])
 def get_events():
-    events = db.session.query(model.Event).all()
+    events = db.session.query(models.Event).all()
     event_list = []
     for event in events:
         event_data = {
@@ -20,7 +20,7 @@ def get_events():
 @bp.route("/event/add", methods=[])
 def add_event():
     added_event = request.get_json()
-    event = model.Event(name=added_event["name"], date=added_event['date'])
+    event = models.Event(name=added_event["name"], date=added_event['date'])
     db.session.add(event)
     db.commit()
     return jsonify({"message": "Event successfully created."})
@@ -28,7 +28,7 @@ def add_event():
 
 @bp.route("/event/delete/<int:event_id>", methods=['POST'])
 def delete_event(event_id):
-    event = db.session.query(model.Event).get(event_id)
+    event = db.session.query(models.Event).get(event_id)
     if not event:
         return jsonify({"message": "Event not found."}), 404
     db.session.delete(event)
@@ -38,7 +38,7 @@ def delete_event(event_id):
 
 @bp.route("/event/<int:event_id>", methods=['GET'])
 def get_event(event_id):
-    event = db.session.query(model.Event).get(event_id)
+    event = db.session.query(models.Event).get(event_id)
     if not event:
         return jsonify({"message": "Event not found."}), 404
     event_data = {
@@ -51,7 +51,7 @@ def get_event(event_id):
 
 @bp.route("/player", methods=['GET'])
 def get_players():
-    players = db.session.query(model.Player).all()
+    players = db.session.query(models.Player).all()
     player_list = []
     for player in players:
         player_data = {
@@ -64,7 +64,7 @@ def get_players():
 @bp.route("/player/add", methods=['POST'])
 def add_player():
     added_player = request.get_json()
-    player = model.Player(name=added_player["name"])
+    player = models.Player(name=added_player["name"])
     db.session.add(player)
     db.session.commit()
     return jsonify({"message": "Player successfully added."})
@@ -72,7 +72,7 @@ def add_player():
 
 @bp.route("/player/delete/<id:player_id>")
 def delete_player(player_id):
-    player = db.session.query(model.Player).get(player_id)
+    player = db.session.query(models.Player).get(player_id)
     if not player:
         return jsonify({"message": "Player not found."}), 404
     db.session.delete(player)
@@ -82,7 +82,7 @@ def delete_player(player_id):
 
 @bp.route("/player/<id:player_id>", methods=['GET'])
 def get_player(player_id):
-    player = db.session.query(model.Player).get(player_id)
+    player = db.session.query(models.Player).get(player_id)
     if not player:
         return jsonify({"message": "Player not found."}), 404
     player_data = {
