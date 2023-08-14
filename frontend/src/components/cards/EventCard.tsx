7f4e74a926e336846e9ba38a998c31d0f7e1b436
sutogabor <wakacorpz@gmail.com>
2023-08-14 15:38:36 +0200

    add additional info and date formatting to EventCard.tsx

--- frontend/src/components/cards/EventCard.tsx
+++ frontend/src/components/cards/EventCard.tsx
@@ -1,19 +1,39 @@
 import React from "react";
-import {EventCardProps} from "../../interfaces/EvenetCardProps.ts";
+import {Event} from "../../interfaces/Event.ts";
+import EventDetailsButton from "../buttons/EventDetailsButton.tsx";
+import DeleteButton from "../buttons/DeleteButton.tsx";
+import useDeleteEvent from "../../hooks/useDeleteEvent.tsx";
+import "../../styles/event-card.css"
 
-const EventCard: React.FC<EventCardProps> = ({ event, deleteEvent }) => {
+
+export interface EventCardProps {
+    event: Event;
+}
+
+
+const EventCard: React.FC<EventCardProps> = ({ event}) => {
+
+    const { deleteEvent, isLoading } = useDeleteEvent();
+    const eventDate = new Date(event.date);
+    const formattedDate = `${eventDate.getFullYear()}.${(eventDate.getMonth() + 1)
+        .toString().padStart(2, '0')}.${eventDate.getDate()
+        .toString().padStart(2, '0')}. ${eventDate.getHours()
+        .toString().padStart(2, '0')}:${eventDate.getMinutes()
+        .toString().padStart(2, '0')}`;
+
+
     return (
-        <div className="card" key={event.id}>
+        <div className="event-card" key={event.id}>
             <div className="card-details">
                 <div className="card-header">{event.name}</div>
-                <div className="card-body">{event.date}</div>
+                <div className="card-body">
+                    <p>Start time:</p>
+                    {formattedDate}
+                </div>
             </div>
             <div className="card-buttons">
-                <button
-                    className="card-button"
-                    onClick={() => deleteEvent(event.id)}
-                    value="Delete"
-                >Delete</button>
+                <EventDetailsButton eventId={event.id} />
+                <DeleteButton onDelete={() => deleteEvent(event.id)} isLoading={isLoading}/>
             </div>
         </div>
     );
