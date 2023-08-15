a98cef1900d2c2a389974ca5ee575c3f06722e00
sutogabor <wakacorpz@gmail.com>
2023-08-15 10:06:26 +0200

    refactor HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
+import logo from './../assets/images/home-page-logo.png'
+
 const Homepage: React.FC = () => {
     return (
         <div className="homepage-container">
             <h1 className="title">Welcome!</h1>
             <div className="picture-container">
-                <img src="../assets/images/home-page-logo.png" alt="Welcome" className="picture" />
+                <img src={logo} alt="Welcome" className="picture" />
             </div>
             <div className="information">
                 <p>This is some information about the page.</p>
