b456adeeb847ce6ad41d378d4990a261684f996e
sutogabor <wakacorpz@gmail.com>
2023-08-15 15:45:40 +0200

    add additional functionality and content to HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,14 +1,29 @@
-import logo from './../assets/images/home-page-logo.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './../assets/images/home-page-logo.png';
+import './../styles/homepage.css';
 
 const Homepage: React.FC = () => {
     return (
         <div className="homepage-container">
-            <h1 className="title">Welcome!</h1>
-            <div className="picture-container">
-                <img src={logo} alt="Welcome" className="picture" />
-            </div>
-            <div className="information">
-                <p>This is some information about the page.</p>
+            <header className="homepage-header">
+                <h1 className="title">Welcome!</h1>
+            </header>
+            <div className="content-container">
+                <Link to="/tournaments">
+                    <img src={logo} alt="Tournaments" className="picture" />
+                </Link>
+                <div className="information">
+                    <div className="information-card">
+                        <p>
+                            <h3>Welcome to my Tournament Organiser project!</h3>
+                            I started this pet project while I was learning programming in CodeCool,
+                            in order the help the organisation of school events such as Ping-Pong
+                            or video game tournaments. Thank you for trying it's features and feel
+                            free to ask me any questions. Enjoy your tournament! May the best win!!
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
