6b2e710924ed99874869f8c1b05e85fcef92e7ad
sutogabor <wakacorpz@gmail.com>
2023-08-21 15:17:05 +0200

    refactor/rename "Event" to "Tournament" in models.py

--- backend/models.py
+++ backend/models.py
@@ -14,15 +14,15 @@ class Player(Base):
     events = relationship("Event", secondary="player_event", back_populates="players")
 
 
-class PlayerEvent(Base):
-    __tablename__ = 'player_event'
+class PlayerTournament(Base):
+    __tablename__ = 'player_tournament'
     id = Column(Integer, Sequence('player_event_id_seq', start=1, increment=1), primary_key=True, autoincrement=True)
     player_id = Column(Integer, ForeignKey('players.id'), primary_key=True)
     event_id = Column(Integer, ForeignKey('events.id'), primary_key=True)
 
 
-class Event(Base):
-    __tablename__ = "events"
+class Tournament(Base):
+    __tablename__ = "tournaments"
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
     date = Column(DateTime)
