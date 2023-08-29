14e8d29d268ef982788c7ef4803b65f4a24a3054
sutogabor <wakacorpz@gmail.com>
2023-08-29 11:02:36 +0200

    add functionality to AddPlayer.tsx

--- frontend/src/pages/AddPlayer.tsx
+++ frontend/src/pages/AddPlayer.tsx
@@ -1,68 +1,102 @@
-import {useEffect, useState} from "react";
-import {Event} from "../interfaces/Event.ts";
+import {useState, useEffect, ChangeEvent} from 'react';
+import './../styles/add-player-page.css';
 import {apiGet, apiPost} from "../utils/apiCalls.ts";
 
-
-const AddPlayer = () => {
-    const [events, setEvents] = useState<Event[]>([]);
-    const [playerName , setPlayerName] = useState('');
-    const [selectedEvents, setSelectedEvents] = useState<number[]>([]);
-
-    const getEvents = async () :void => {
-        const response :Promise<Event[]> = await apiGet('/event');
-        setEvents(response);
-    }
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        apiPost({name: playerName, eventIds: selectedEvents}, '/player/add', 'Player created successfully');
-        window.location.reload()
-    }
+const PlayerRegistrationPage = () => {
+    const [playerName, setPlayerName] = useState('');
+    const [selectedTournaments, setSelectedTournaments] = useState<number[]>([]);
+    const [tournamentList, setTournamentList] = useState<{ id: number; name: string }[]>([]);
 
 
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        const itemValue: number = Number(event.target.value);
-
-        if (event.target.checked) {
-            if (!selectedEvents.includes(itemValue)) {
-                setSelectedEvents([...selectedEvents, itemValue]);
+    async function getData() {
+        try {
+            const response = await apiGet("https://localhost:8080/event");
+            if (response && response.ok) {
+                const data = await response.json();
+                return data;
+            } else {
+                console.log("Bad response at AddPlayer page useEffect!")
             }
-        } else {
-            const updatedItems: number[] = selectedEvents.filter((item) => item !== itemValue);
-            setSelectedEvents(updatedItems);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    useEffect(() => {
+        getData().then(data => setTournamentList(data))
+    }, []);
+
+    const handlePlayerNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setPlayerName(event.target.value);
+    };
+
+    const handleTournamentToggle = (tournamentId: number) => {
+        setSelectedTournaments((prevSelected) =>
+            prevSelected.includes(tournamentId)
+                ? prevSelected.filter((id) => id !== tournamentId)
+                : [...prevSelected, tournamentId]
+        );
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const playerPayload = {
+            name: playerName,
+            tournaments: selectedTournaments
+        };
+        const playerURL = 'https://localhost:8080/player';
+        const successMessage = "Player added and registered successfully.";
+
+        try {
+            await apiPost(playerPayload, playerURL, successMessage);
+            console.log("Player added and registered successfully.");
+        } catch (error) {
+            console.log(error);
         }
     };
 
-    useEffect(()=> {
-        getEvents();
-    },[])
 
     return (
-        <div>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        value={playerName}
-                        onChange={(e) => setPlayerName(e.target.value)}
-                        placeholder="Player name"
-                    />
-
-                        {events.map((event:Event)=> (
-                            <div>
-                            <input type="checkbox" onChange={handleChange} value={event.id} id={event.name}/>
-                            <label htmlFor={event.name}>{event.name}</label>
-                            </div>
-                        ))}
-
-                    <button type="submit">Add</button>
-                </form>
-            </div>
-
+        <div className="registration-container">
+            <h2>Player Registration</h2>
+            <form>
+                <div className="form-group">
+                    <label className="label-text" htmlFor="playerName">
+                        Player Name:
+                    </label>
+                    <div className="input-container">
+                        <input
+                            type="text"
+                            id="playerName"
+                            value={playerName}
+                            onChange={handlePlayerNameChange}
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="tournament-list">
+                    <p className="choose-tournaments-text">Choose your tournaments:</p>
+                    {tournamentList.map((tournament) => (
+                        <div key={tournament.id} className="tournament-item">
+                            <label className="tournament-name">{tournament.name}</label>
+                            <input
+                                type="checkbox"
+                                id={`tournament-${tournament.id}`}
+                                value={tournament.id}
+                                checked={selectedTournaments.includes(tournament.id)}
+                                onChange={() => handleTournamentToggle(tournament.id)}
+                            />
+                        </div>
+                    ))}
+                </div>
+                <button type="button" onClick={handleSubmit} className="submit-button">
+                    Register
+                </button>
+            </form>
         </div>
-    )
+    );
+};
 
-}
-
-export default AddPlayer;
+export default PlayerRegistrationPage;
