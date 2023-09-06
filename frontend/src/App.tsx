5f143da977e04513a56d088deef0c0a7de02be7e
sutogabor <wakacorpz@gmail.com>
2023-09-06 11:30:34 +0200

    add layout to routing in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,34 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Header from "./components/Header.tsx";
-import HomePage from "./pages/HomePage.tsx";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header.tsx';
+import HomePage from './pages/HomePage.tsx';
+import Tournaments from './pages/Tournaments.tsx';
+import EventDetails from './pages/EventDetails.tsx';
+import AddPlayer from './pages/AddPlayer.tsx';
+import Footer from './components/Footer.tsx';
 import AddTournament from "./pages/AddTournament.tsx";
-import Tournaments from "./pages/Tournaments.tsx";
-import EventDetails from "./pages/EventDetails.tsx";
-import AddPlayer from "./pages/AddPlayer.tsx";
-import Footer from "./components/Footer.tsx";
 
+// Define a layout component that includes the header and footer
+const Layout = ({ children }: { children: React.ReactNode }) => (
+    <div>
+        <Header />
+        {children}
+        <Footer />
+    </div>
+);
 
 function App() {
-
-
     return (
-        <div>
-            <Router>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<HomePage />}/>
-                    <Route path="/event-details/:id" element={ <EventDetails />}/>
-                    <Route path="/add-tournament" element={ <AddTournament />}/>
-                    <Route path="/tournaments" element={ <Tournaments /> }/>
-                    <Route path="/add-player" element={ <AddPlayer /> }/>
-                </Routes>
-                <Footer/>
-            </Router>
-        </div>
-    )
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout><HomePage /></Layout>} />
+                <Route path="/event-details/:id" element={<Layout><EventDetails /></Layout>} />
+                <Route path="/add-tournament" element={<Layout><AddTournament /></Layout>} />
+                <Route path="/tournaments" element={<Layout><Tournaments /></Layout>} />
+                <Route path="/add-player" element={<Layout><AddPlayer /></Layout>} />
+            </Routes>
+        </Router>
+    );
 }
 
-
-export default App
+export default App;
