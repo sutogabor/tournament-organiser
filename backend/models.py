58251f3969ad743a19058a8b0dcbfeabf46fd9e0
sutogabor <wakacorpz@gmail.com>
2023-09-12 10:11:00 +0200

    refactor models.py changing declarative base method to db.Model

--- backend/models.py
+++ backend/models.py
@@ -1,20 +1,19 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Sequence
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 
 
-Base = declarative_base()
 db = SQLAlchemy()
 
 
-class Player(Base):
+class Player(db.Model):
     __tablename__ = "players"
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
     tournaments = relationship("Tournament", secondary="player_tournament", back_populates="players")
 
 
-class Tournament(Base):
+class Tournament(db.Model):
     __tablename__ = "tournaments"
     id = Column(Integer, primary_key=True)
     name = Column(String(255))
@@ -23,14 +22,14 @@ class Tournament(Base):
     matches = relationship("Match")
 
 
-class PlayerTournament(Base):
+class PlayerTournament(db.Model):
     __tablename__ = 'player_tournament'
     id = Column(Integer, Sequence('player_tournament_id_seq', start=1, increment=1), primary_key=True, autoincrement=True)
     player_id = Column(Integer, ForeignKey('players.id'), primary_key=True)
     tournament_id = Column(Integer, ForeignKey('tournaments.id'), primary_key=True)
 
 
-class Match(Base):
+class Match(db.Model):
     __tablename__ = "matches"
     id = Column(Integer, primary_key=True)
     tournament_id = Column(Integer, ForeignKey("tournaments.id"))
@@ -47,4 +46,7 @@ class Match(Base):
     player_2 = relationship("Player", foreign_keys=[player_2_id])
     winner = relationship("Player", foreign_keys=[winner_id])
     next_match = relationship("Match", foreign_keys=[next_match_id], remote_side=[id])
+
+    def __str__(self):
+        return f"Match ID: {self.id}, Event ID: {self.tournament_id}, Player 1 ID: {self.player_1_id}, Player 2 ID: {self.player_2_id}, Next Match ID: {self.next_match_id}"
 
