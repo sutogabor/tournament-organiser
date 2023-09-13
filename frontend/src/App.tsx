a65aa439e148198cdfd0610386adb7726e600708
sutogabor <wakacorpz@gmail.com>
2023-09-13 11:08:16 +0200

    rename event-details rout in App.tsx to tournament-details route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
                 <Header />
                 <Routes>
                     <Route path="/" element={<HomePage />}/>
-                    <Route path="/event-details/:id" element={ <TournamentDetails />}/>
+                    <Route path="/tournament-details/:id" element={ <TournamentDetails />}/>
                     <Route path="/add-tournament" element={ <AddTournament />}/>
                     <Route path="/tournaments" element={ <Tournaments /> }/>
                     <Route path="/add-player" element={ <AddPlayer /> }/>
