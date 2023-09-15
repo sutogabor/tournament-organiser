12a099c7110e3f13770975dfb9c24793bb1db0dc
sutogabor <wakacorpz@gmail.com>
2023-09-15 13:23:19 +0200

    add MatchParticipant model and costum __str__ function to models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Sequence
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Sequence, Boolean
 from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
-
 
 db = SQLAlchemy()
 
@@ -27,19 +26,10 @@ class Tournament(db.Model):
     matches = relationship("Match")
 
 
-class PlayerTournament(db.Model):
-    __tablename__ = 'player_tournament'
-    id = Column(Integer, Sequence('player_tournament_id_seq', start=1, increment=1), primary_key=True, autoincrement=True)
-    player_id = Column(Integer, ForeignKey('players.id'), primary_key=True)
-    tournament_id = Column(Integer, ForeignKey('tournaments.id'), primary_key=True)
-
-
 class Match(db.Model):
     __tablename__ = "matches"
     id = Column(Integer, primary_key=True)
     tournament_id = Column(Integer, ForeignKey("tournaments.id"))
-    player_1_id = Column(Integer, ForeignKey("players.id"))
-    player_2_id = Column(Integer, ForeignKey("players.id"))
     winner_id = Column(Integer, ForeignKey("players.id"), nullable=True)
     state = Column(String(20))
     start_time = Column(DateTime)
@@ -48,11 +38,36 @@ class Match(db.Model):
 
     participants = relationship("Player", secondary="match_participants", back_populates="matches")
     tournament = relationship("Tournament", back_populates="matches")
-    player_1 = relationship("Player", foreign_keys=[player_1_id])
-    player_2 = relationship("Player", foreign_keys=[player_2_id])
     winner = relationship("Player", foreign_keys=[winner_id])
     next_match = relationship("Match", foreign_keys=[next_match_id], remote_side=[id])
 
     def __str__(self):
-        return f"Match ID: {self.id}, Event ID: {self.tournament_id}, Player 1 ID: {self.player_1_id}, Player 2 ID: {self.player_2_id}, Next Match ID: {self.next_match_id}"
+        participants = [player.name for player in self.participants if player]
+        participants_str = ", ".join(participants) if participants else "No participants"
+
+        return (f"Match ID: {self.id}, Tournament ID: {self.tournament_id}, "
+                f"Winner ID: {self.winner_id}, State: {self.state}, "
+                f"Start Time: {self.start_time}, Round: {self.tournament_round_text}, "
+                f"Next Match ID: {self.next_match_id}, Participants: {participants_str}")
+
+
+class PlayerTournament(db.Model):
+    __tablename__ = "player_tournament"
+    id = Column(Integer, Sequence('player_tournament_id_seq', start=1, increment=1), primary_key=True,
+                autoincrement=True)
+    player_id = Column(Integer, ForeignKey('players.id'), primary_key=True)
+    tournament_id = Column(Integer, ForeignKey('tournaments.id'), primary_key=True)
+
+
+class MatchParticipant(db.Model):
+    __tablename__ = "match_participants"
+    id = Column(Integer, Sequence('match_participants_id_seq', start=1, increment=1), primary_key=True, autoincrement=True)
+    match_id = Column(Integer, ForeignKey('matches.id'), primary_key=True)
+    player_id = Column(Integer, ForeignKey('players.id'), primary_key=True)
+    result_text = Column(String(50), nullable=True)
+    isWinner = Column(String(50), nullable=True)
+    status = Column(String(20), nullable=True)
+
+
+
 
