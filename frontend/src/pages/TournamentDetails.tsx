1317245c5c75a14355b3cad2f4a636377a7f401c
sutogabor <wakacorpz@gmail.com>
2023-09-18 12:52:21 +0200

    add matchesData mock data to TournamentDetails.tsx

--- frontend/src/pages/TournamentDetails.tsx
+++ frontend/src/pages/TournamentDetails.tsx
@@ -8,26 +8,206 @@ const TournamentDetails: React.FC = () => {
   const [details, setDetails] = useState<Event | undefined>();
   const { id } = useParams<TournamentDetailsParams>();
 
-  const fetchEvent = async () => {
-    try {
-      const response = await fetch(`/event/${id}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        const event: Event = data as Event; // Assuming the response data matches the Event interface
-        setDetails(event);
-      } else {
-        console.error('Event not found!!');
-      }
-    } catch (error) {
-      console.error('Error occurred while fetching event:', error);
-    }
-  };
+const matchesData = [
+    {
+        "id": 10001,
+        "name": "NAME",
+        "nextMatchId": 10101,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "c016cb2a-fdd9-4c40-a81f-0cc6bdf4b9cc",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Zoli"
+            },
+            {
+                "id": "9ea9ce1a-4794-4553-856c-9a3620c0531b",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Peti"
+            }
+        ]
+    },
+    {
+        "id": 10002,
+        "name": "NAME",
+        "nextMatchId": 10102,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "c016cb2a-fdd9-4c57-a81f-0cc6bdf4b9cc",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Imi"
+            },
+            {
+                "id": "9ea9ce1a-4794-7153-856c-9a3620c0531b",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Feri"
+            }
+        ]
+    },
+    {
+        "id": 10003,
+        "name": "NAME",
+        "nextMatchId": 10103,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "c016cb2a-fdd9-4c40-a54f-0cc6bdf4b9cc",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Geza"
+            },
+            {
+                "id": "9ea9ce1a-4729-4553-856c-9a3620c0531b",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Roli"
+            }
+        ]
+    },
+    {
+        "id": 10004,
+        "name": "NAME",
+        "nextMatchId": 10104,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "c088cb2a-fdd9-4c40-a81f-0cc6bdf4b9cc",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Liza"
+            },
+            {
+                "id": "9ea9ce1a-4345-4553-856c-9a3620c0531b",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Karcsi"
+            }
+        ]
+    },
+    {
+        "id": 10005,
+        "name": "NAME",
+        "nextMatchId": 10104,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "c016cb2a-fdd9-4c40-a23f-0cc6bdf4b9cc",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Kata"
+            },
+            {
+                "id": "9ea9ce1a-4794-4553-958c-9a3620c0531b",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Eszter"
+            }
+        ]
+    },
+    {
+        "id": 10101,
+        "name": "NAME",
+        "nextMatchId": 10201,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "c016cb2a-fdd9-4c50-a77f-0cc6bdf4b9cc",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Samu"
+            }
+        ]
+    },
+    {
+        "id": 10102,
+        "name": "NAME",
+        "nextMatchId": 10201,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "9ea9ce1a-4794-3333-856c-9a3620c0531b",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Ati"
+            }
+        ]
+    },
+    {
+        "id": 10103,
+        "name": "NAME",
+        "nextMatchId": 10202,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": [
+            {
+                "id": "9ea9ce1a-5566-3353-856c-9a3620c0531b",
+                "resultText": null,
+                "isWinner": false,
+                "status": null,
+                "name": "Ede"
+            }
+        ]
+    },
+    {
+        "id": 10104,
+        "name": "NAME",
+        "nextMatchId": 10202,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": []
+    },
+    {
+        "id": 10201,
+        "name": "NAME",
+        "nextMatchId": 10301,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": []
+    },
+    {
+        "id": 10202,
+        "name": "NAME",
+        "nextMatchId": 10301,
+        "tournamentRoundText": "0",
+        "startTime": "2024-05-30T14:15:00",
+        "state": "DONE",
+        "participants": []
+    },
+]
 
   useEffect(() => {
     fetchEvent();
