e9c1564521a8255f782afc746f0fdcfd4a0208cf
sutogabor <wakacorpz@gmail.com>
2023-10-04 10:53:46 +0200

    refactor get_tournaments function in routes.py

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -7,10 +7,9 @@ from api.database.database_handler import db
 
 routes_bp = Blueprint("routes", __name__)
 
-
-@routes_bp.route("/tournament", methods=['GET'])
+@routes_bp.route("/tournaments", methods=['GET'])
 def get_tournaments():
-    tournaments = models.Tournament.query.all()
+    tournaments = Tournament.query.all()
     tournament_list = []
     for tournament in tournaments:
         tournament_data = {
@@ -21,8 +20,8 @@ def get_tournaments():
             'matches': [
                 {
                     'match_id': match.id,
-                    'player_1': match.player_1.name,
-                    'player_2': match.player_2.name,
+                    'player_1': match.participants[0].name if match.participants and len(match.participants) >= 1 else None,
+                    'player_2': match.participants[1].name if match.participants and len(match.participants) >= 2 else None,
                     'winner': match.winner.name if match.winner else None
                 }
                 for match in tournament.matches
